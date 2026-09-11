f55a771812889b1b32bca249338288fb2665656e
Araaf Royall ❂ <87515236+AraafRoyall@users.noreply.github.com>
2026-09-11 10:00:39 +0530

    Update WorkProfileScreen.kt

--- app/src/main/java/com/bintianqi/owndroid/feature/work_profile/WorkProfileScreen.kt
+++ app/src/main/java/com/bintianqi/owndroid/feature/work_profile/WorkProfileScreen.kt
@@ -124,6 +124,7 @@ fun DeleteWorkProfileScreen(
     var flags by remember { mutableIntStateOf(0) }
     var warning by remember { mutableStateOf(false) }
     var reason by remember { mutableStateOf("") }
+    var confirmation by remember { mutableStateOf("") }
     MyScaffold(R.string.delete_work_profile, onNavigateUp) {
         CheckBoxItem(R.string.wipe_external_storage, flags and WIPE_EXTERNAL_STORAGE != 0) {
             flags = flags xor WIPE_EXTERNAL_STORAGE
@@ -146,6 +147,7 @@ fun DeleteWorkProfileScreen(
         Spacer(Modifier.padding(vertical = 5.dp))
         Button(
             {
+                confirmation = ""
                 warning = true
             },
             Modifier.fillMaxWidth(),
@@ -162,7 +164,13 @@ fun DeleteWorkProfileScreen(
                 Text(stringResource(R.string.warning))
             },
             text = {
-                Text(stringResource(R.string.wipe_work_profile_warning))
+                OutlinedTextField(
+                    confirmation,
+                    { confirmation = it },
+                    Modifier.fillMaxWidth(),
+                    label = { Text("Enter DELETE to continue") },
+                    placeholder = { Text("DELETE") }
+                )
             },
             onDismissRequest = { warning = false },
             confirmButton = {
@@ -178,7 +186,7 @@ fun DeleteWorkProfileScreen(
                     {
                         vm.deleteProfile(flags, reason)
                     },
-                    enabled = timer == 0,
+                    enabled = timer == 0 && confirmation == "DELETE",
                     colors = ButtonDefaults.textButtonColors(contentColor = colorScheme.error)
                 ) {
                     Text(stringResource(R.string.confirm) + timerText)
